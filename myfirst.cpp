4927de4a0f20162a2be84a6642980540af55d683
pnp472002 <138452505+pnp472002@users.noreply.github.com>
2023-08-26 00:06:36 +0530

    Add files via upload

---
+++ myfirst.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Prajot
+{
+    int longestsubarray(string a)
+    {
+
+    }
+};
+int main()
+{
+    cout<<"Eneter the string to find longest subarray within it,without repeating alpabets";
+    string a;
+    cin>>a;
+    cout<<longestsubarray(a);
+    return 0;
+}
